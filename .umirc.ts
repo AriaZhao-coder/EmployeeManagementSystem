91587d881af2b21367e503119c9593a8aded3c85
AriaZhao-coder <yizhao1027@163.com>
2025-02-06 10:08:13 +0800

    用户数据01

--- .umirc.ts
+++ .umirc.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'umi';
 import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: ['@umijs/plugins/dist/model'],
   npmClient: 'pnpm',
   alias: {
     utils: resolve(__dirname, './src/utils'),
     components: resolve(__dirname, './src/components'),
   },
-
   fastRefresh: true,  // 设置为布尔值 true 或 false
 });
 
---
+++ src/pages/dashboard/component/OldStaffTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function OldStaffTable(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default OldStaffTable;
